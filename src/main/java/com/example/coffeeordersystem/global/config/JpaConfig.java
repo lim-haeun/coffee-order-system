1712120f8317139937daac9dc66610330f7d83f0
임하은 <gkdmsdl100414@gmail.com>
2026-05-11 07:21:53 +0900

    feat: BaseEntity 추가

--- src/main/java/com/example/coffeeordersystem/global/config/JpaConfig.java
+++ src/main/java/com/example/coffeeordersystem/global/config/JpaConfig.java
@@ -1,7 +1,9 @@
 package com.example.coffeeordersystem.global.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @Configuration
 public class JpaConfig {
 }
