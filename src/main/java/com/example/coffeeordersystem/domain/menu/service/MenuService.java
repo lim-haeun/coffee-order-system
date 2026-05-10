a301920a31bd44bbaac6d8446b7377f7c8b05b73
임하은 <gkdmsdl100414@gmail.com>
2026-05-11 07:55:59 +0900

    feat: 서비스 클래스에 @Transactional(readOnly = true) 추가

--- src/main/java/com/example/coffeeordersystem/domain/menu/service/MenuService.java
+++ src/main/java/com/example/coffeeordersystem/domain/menu/service/MenuService.java
@@ -11,8 +11,10 @@ import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
 public class MenuService {
 
     private final MenuRepository menuRepository;
--- src/main/java/com/example/coffeeordersystem/domain/order/service/OrderService.java
+++ src/main/java/com/example/coffeeordersystem/domain/order/service/OrderService.java
@@ -23,6 +23,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
 public class OrderService {
 
     private final UserRepository userRepository;
