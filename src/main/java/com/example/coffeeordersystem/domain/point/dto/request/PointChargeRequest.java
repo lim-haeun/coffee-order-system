973c4ba204236e8817af3392c38dc25cc439829b
임하은 <gkdmsdl100414@gmail.com>
2026-05-11 06:44:54 +0900

    feat: 포인트 충전 요청의 유효성 검사 메시지 추가

--- src/main/java/com/example/coffeeordersystem/domain/point/dto/request/PointChargeRequest.java
+++ src/main/java/com/example/coffeeordersystem/domain/point/dto/request/PointChargeRequest.java
@@ -4,7 +4,12 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 public record PointChargeRequest(
-        @NotNull @Positive Long userId,
-        @NotNull @Positive Long amount
+        @NotNull(message = "사용자 ID는 필수입니다.")
+        @Positive(message = "사용자 ID는 0보다 커야 합니다.")
+        Long userId,
+
+        @NotNull(message = "충전 금액은 필수입니다.")
+        @Positive(message = "충전 금액은 0보다 커야 합니다.")
+        Long amount
 ) {
 }
