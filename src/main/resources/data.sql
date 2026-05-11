61ab987f50fc6abc3280c2c0be5593d31a21a7cf
임하은 <gkdmsdl100414@gmail.com>
2026-05-11 09:39:21 +0900

    chore: data.sql 기반 유저 초기 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,3 +9,6 @@ VALUES ('바닐라라떼', 5500, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 
 INSERT INTO menus (name, price, created_at, updated_at)
 VALUES ('밀크티', 6000, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+INSERT INTO users (name, created_at, updated_at)
+VALUES ('test-user', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
