7588ec4bcfdad6659a0179920cc55e357ee6007c
임하은 <gkdmsdl100414@gmail.com>
2026-05-11 09:28:18 +0900

    chore: data.sql 기반 메뉴 초기 데이터 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO menus (name, price, created_at, updated_at)
+VALUES ('아메리카노', 4500, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+INSERT INTO menus (name, price, created_at, updated_at)
+VALUES ('카페라떼', 5000, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+INSERT INTO menus (name, price, created_at, updated_at)
+VALUES ('바닐라라떼', 5500, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+
+INSERT INTO menus (name, price, created_at, updated_at)
+VALUES ('밀크티', 6000, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
